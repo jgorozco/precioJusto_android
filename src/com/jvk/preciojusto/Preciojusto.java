ce71dd87446bb625a753cab79adc83ce70bd4cb5
jose garcia orozco <josegoupm@gmail.com>
2011-11-05 18:27:58 +0100

    fist commit, create proy

---
+++ src/com/jvk/preciojusto/Preciojusto.java
@@ -0,0 +1,13 @@
+package com.jvk.preciojusto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Preciojusto extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
